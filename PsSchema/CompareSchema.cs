c4d58e22da5b227d3ead144aaa9fcd1e61c319a4
Kenneth Johnson <Kenjohnson777@hotmail.com>
2018-09-03 11:17:07 -0400

    Created stubs for powershell cmdlets

---
+++ PsSchema/CompareSchema.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace PsSchema
+{
+    [Cmdlet(VerbsData.Compare, "Schema")]
+    public class CompareSchema : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+        }
+    }
+}
---
+++ PsSchema/SaveSchema.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Management.Automation;
+
+namespace PsSchema
+{
+    [Cmdlet(VerbsData.Save, "Schema")]
+    public class SaveSchema : PSCmdlet
+    {
+        protected override void ProcessRecord()
+        {
+        }
+    }
+}
